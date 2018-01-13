ac6830d92f384f6038109a1c1f636b6ae68cde45
iroberts88 <iroberts88@gmail.com>
2018-01-13 15:03:23 -0500

    update

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ var rc = null,
     ge = null;
 
 //{endpoint: "https://dynamodb.us-west-1.amazonaws.com"}
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
-});
+AWS.config.region = process.env.REGION;
 
 function init() {
 
