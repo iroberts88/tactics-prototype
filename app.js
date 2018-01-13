5d868a1b95d6cb9d6ebec9971293627cdbe251ea
iroberts88 <iroberts88@gmail.com>
2018-01-13 14:53:17 -0500

    Update
    AWS 1.0

---
+++ app.js
@@ -0,0 +1,133 @@
+var app = require('http').createServer(webResponse),
+    fs = require('fs'),
+    AWS = require("aws-sdk"),
+    io = require('socket.io').listen(app),
+    GameEngine = require('./gameengine.js').GameEngine,
+    RequireCheck = require('./requirecheck.js').RequireCheck;
+
+const crypto = require('crypto');
+    
+var rc = null,
+    ge = null;
+
+//{endpoint: "https://dynamodb.us-west-1.amazonaws.com"}
+AWS.config.update({
+  region: "us-east-1",
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+});
+
+function init() {
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    rc = new RequireCheck();
+    ge = new GameEngine();
+    rc.onReady(onReady);
+
+    // ----------------------------------------------------------
+    // Start Database Connection
+    // ----------------------------------------------------------
+
+    rc.ready();
+    rc.require('dbMaps','dbUsers','dbItems','dbBuffs','dbClasses');
+
+    // ---- Load Maps ----
+    docClient.scan({TableName: 'tactics_maps'}, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Loading maps... " + data.Items.length + ' found');
+            ge.loadMaps(data.Items);
+            rc.ready('dbMaps');
+        }
+    });
+    // ---- Load Items ----
+    docClient.scan({TableName: 'tactics_items'}, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Loading items... " + data.Items.length + ' found');
+            ge.loadItems(data.Items);
+            rc.ready('dbItems');
+        }
+    });
+    // ---- Load Buffs ----
+    docClient.scan({TableName: 'tactics_buffs'}, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Loading buffs... " + data.Items.length + ' found');
+            ge.loadBuffs(data.Items);
+            rc.ready('dbBuffs');
+        }
+    });
+
+    // ---- Load Classes ----
+    docClient.scan({TableName: 'tactics_classes'}, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Loading classes... " + data.Items.length + ' found');
+            ge.loadClasses(data.Items);
+            rc.ready('dbClasses');
+        }
+    });
+
+    // ---- Load Userbase ----
+    docClient.scan({TableName: 'users'}, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Loading users... " + data.Items.length + ' found');
+            ge.loadUsers(data.Items);
+            rc.ready('dbUsers');
+        }
+    });
+
+}
+init();
+
+
+
+// ----------------------------------------------------------
+// Start Web Server
+// ----------------------------------------------------------
+app.listen(8088);
+
+function webResponse(req, res) {
+    var filename = req.url;
+
+    // Check for default
+    if (filename == '/') {
+        filename = '/index.html';
+    }
+
+    //console.log('HTTP Request: ' + filename);
+
+    fs.readFile(__dirname + '/public' + filename, function(err, data) {
+        if (err) {
+            console.log('Couldn\'t find file: ' + req.url);
+            res.writeHead(500);
+            res.end('Couldn\'t find file: ' + req.url)
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+function onReady() {
+    console.log('All require items loaded. Starting Game Engine');
+    ge.init();
+}
+
+
+// TO DO: Need to keep track of sockets with ids
+// ----------------------------------------------------------
+// Start Socket Listener
+// ----------------------------------------------------------
+io.sockets.on('connection', ge.newConnection);
+
+console.log('Listening');
+
+
---
+++ gameengine.js
@@ -0,0 +1,262 @@
+//----------------------------------------------------------------
+//gameengine.js
+//----------------------------------------------------------------
+
+var GameSession = require('./gamesession.js').GameSession,
+    Player = require('./player.js').Player;
+
+var self = null;
+
+var GameEngine = function() {
+    this.users = {};
+    this._userIndex = {};
+    this.gameTickInterval = 20;
+    this.lastTime = Date.now();
+    this.sessions = {}; //List of currently active gameSessions
+    this.sessionList = [];
+    this.players = {}; //List of players that do not have a gameSession
+    this.playersWaiting = [];
+    this.openSessions = null;
+    //database objects
+    this.maps = [];
+    this.items = {};
+    this.buffs = {};
+    this.classes = {};
+    this.abilityIndex = {};
+    this.playerCount = 0;
+    //variables for ID's
+    this.idIterator = 0;
+    this.possibleIDChars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyz";
+
+    //TODO this should be randomly generated for each new user?
+    this.hashSalt = 'Salt makes things taste better';
+
+    this.debugList = {}; //used avoid multiple debug chains in tick()
+}
+
+GameEngine.prototype.init = function () {
+    this.start();
+};
+
+GameEngine.prototype.start = function () {
+    console.log('Starting Game Engine.');
+    console.log('Ready. Waiting for players to connect...');
+    self = this;
+    setInterval(this.tick, this.gameTickInterval);
+}
+
+GameEngine.prototype.tick = function() {
+    var now = Date.now();
+    var deltaTime = (now-self.lastTime) / 1000.0;
+    
+    //update all game sessions
+    for (var i in self.sessions){
+        self.sessions[i].tick(deltaTime);
+    }
+
+    //check waiting players and join new sessions
+    if (self.playersWaiting.length >= 2){
+        var s = self.createSession();
+        self.joinSession(s.id,self.players[self.playersWaiting[0]]);
+        self.joinSession(s.id,self.players[self.playersWaiting[0]]);
+        self.sessions[s.id].gameStart();
+    }
+
+    //update debug list
+    for (var k in self.debugList){
+        self.debugList[k].t -= deltaTime;
+        if (self.debugList[k].t <= -5.0){
+            //debug hasnt been updated in 5 seconds
+            //remove from debug list
+            console.log('deleting debug with id ' + self.debugList[k].id);
+            delete self.debugList[k];
+        }
+    }
+    self.emit();
+    self.clearQueue();
+    self.lastTime = now;
+}
+
+GameEngine.prototype.getId = function() {
+    var id = this.idIterator + 'x';
+    for(var i=0; i<3; i++){
+        id += this.possibleIDChars.charAt(Math.floor(Math.random() * this.possibleIDChars.length));
+    }
+    this.idIterator += 1;
+    return id;
+}
+
+// ----------------------------------------------------------
+// Database loading functions
+// ----------------------------------------------------------
+
+GameEngine.prototype.loadMaps = function(arr) {
+    for (var i = 0; i < arr.length;i++){
+        this.maps.push(arr[i].mapid);
+    }
+    console.log('loaded ' + arr.length + ' Maps from db');
+}
+
+GameEngine.prototype.loadItems = function(arr) {
+    for (var i = 0; i < arr.length;i++){
+        this.items[arr[i].itemid] = arr[i];
+    }
+    console.log('loaded ' + arr.length + ' Items from db');
+}
+
+GameEngine.prototype.loadBuffs = function(arr) {
+    for (var i = 0; i < arr.length;i++){
+        this.buffs[arr[i].buffid] = arr[i];
+    }
+    console.log('loaded ' + arr.length + ' Buffs from db');
+}
+
+GameEngine.prototype.loadClasses = function(arr) {
+    this.abilityIndex = {};
+    for (var i = 0; i < arr.length;i++){
+        this.classes[arr[i].classid] = arr[i];
+        //fill in the abiliy index for quick access
+        for (var j = 0; j < this.classes[arr[i].classid].abilities.length;j++){
+            this.abilityIndex[this.classes[arr[i].classid].abilities[j].id] = [this.classes[arr[i].classid].classid,j];
+        }
+    }
+    console.log('loaded ' + arr.length + ' Classes from db');
+}
+
+GameEngine.prototype.loadUsers = function(arr) {
+    for (var i = 0;i < arr.length;i++){
+        this.users[arr[i].username] = arr[i];
+        this.users[arr[i].username].loggedin = false;
+        this._userIndex[arr[i].username] = arr[i].username;
+    }
+    console.log("loaded " + (i) + ' users from db');
+}
+
+// ----------------------------------------------------------
+// Session Functions
+// ----------------------------------------------------------
+
+GameEngine.prototype.createSession = function(){
+    var session = new GameSession(this);
+    session.init({
+        sid: this.getId()
+    });
+    this.sessions[session.id] = session;
+    this.sessionList.push(session.id);
+    return session;
+}
+
+GameEngine.prototype.joinSession = function(id,p) {
+    //join a player <p> to a session and remove from session
+    this.sessions[id].addPlayer(p);
+    this.removePlayer(p);
+}
+
+GameEngine.prototype.leaveSession = function(id,p) {
+    //remove a player <p> from a session and add back to engine
+    this.addPlayer(p);
+    this.sessions[id].removePlayer(p);
+}
+
+GameEngine.prototype.removeSession = function(id) {
+    //delete session with <id>
+    delete this.sessions[id];
+}
+
+GameEngine.prototype.addPlayer = function(p){
+    this.players[p.id] = p;
+    this.playerCount += 1;
+}
+
+GameEngine.prototype.removePlayer = function(p){
+    this.playerLogout(p);
+    delete this.players[p.id];
+    this.playerCount -= 1;
+}
+
+GameEngine.prototype.playerLogout = function(p){
+    this.playerCancelSearch(p);
+}
+GameEngine.prototype.playerCancelSearch = function(p){
+    for (var i = 0; i < this.playersWaiting.length; i++){
+        if (this.playersWaiting[i] == p.id){
+            this.playersWaiting.splice(i,1);
+        }
+    }
+}  
+
+// ----------------------------------------------------------
+// Socket Functions
+// ----------------------------------------------------------
+
+GameEngine.prototype.newConnection = function(socket) {
+    console.log('New Player Connected');
+    console.log('Socket ID: ' + socket.id);
+    //Initialize new player
+    var p = new Player();
+    p.id = self.getId();
+    p.setGameEngine(self);
+    console.log('Player ID: ' + p.id);
+    p.init({socket:socket});
+    self.queuePlayer(p,'connInfo', {mapNames: self.maps});
+    self.addPlayer(p);
+}
+
+GameEngine.prototype.emit = function() {
+    try{
+        for(var i in this.players) {
+            if (this.players[i].netQueue.length > 0){
+                this.players[i].socket.emit('serverUpdate', this.players[i].netQueue);
+            }
+        }
+    }catch(e){
+        try{
+            console.log(this.players[i].netQueue);
+        }catch(e){}
+        console.log(e);
+        console.log(i);
+    }
+}
+GameEngine.prototype.clearQueue = function() {
+    for(var i in this.players) {
+        this.players[i].netQueue = [];
+    }
+}
+
+//Queue data to all players in the session
+GameEngine.prototype.queueData = function(c, d) {
+    var data = { call: c, data: d};
+    for(var i in this.players) {
+        this.players[i].netQueue.push(data);
+    }
+}
+//Queue data to a specific player
+GameEngine.prototype.queuePlayer = function(player, c, d) {
+    var data = { call: c, data: d};
+    player.netQueue.push(data);
+}
+
+//Queue DEBUG data to a specific player
+GameEngine.prototype.debug = function(player, d) {
+    var data = { call: 'debug', data: d};
+    if (typeof this.debugList[d.id] == 'undefined'){
+        //new debug error
+        //add to debug list and send to client
+        this.debugList[d.id] = {
+            id: d.id,
+            n: 1,
+            t: 1.0
+        }
+        data.data.n = 1;
+        player.netQueue.push(data);
+    }else{
+        this.debugList[d.id].n += 1;
+        data.data.n = this.debugList[d.id].n
+        if (this.debugList[d.id].t <= 0){
+            player.netQueue.push(data);
+            this.debugList[d.id].t = 1.0;
+        }
+    }
+}
+
+exports.GameEngine = GameEngine;
